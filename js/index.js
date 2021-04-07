d574952b52ff93d75b33e2a7da3586e4ee9841bc
andresgenda <54486269+andresgenda@users.noreply.github.com>
2021-04-07 15:07:27 -0700

    Added content to JS and modified CSS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 //LAB5
+
+
+$('.agregar').on('click', function(ev){
+    ev.preventDefault();
+    
+    var objeto = $('#newText').val();
+
+    var agregaObj = $('<div class="lis"><p class="itemShop">' + objeto + 
+    '</p><button class="checar">check</button><button class="del">delete</button></div>');
+    
+    $('.Lista').append(agregaObj);
+});
+
+$('.Lista').on('click', '.checar', function(ev){
+    ev.preventDefault();
+
+    $(this).siblings('.itemShop').toggleClass('chec');
+});
+
+$('.Lista').on('click', '.del', function(ev){
+    ev.preventDefault();
+
+    $(this).parent().remove();
+});
+
+
